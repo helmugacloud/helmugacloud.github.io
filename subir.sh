d09335f79c9e5f08903c6bf3339063d1d52d91c7
root <root@helmuga.cloud>
2024-10-30 12:20:01 +0000

    Initial commit

--- subir.sh
+++ subir.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 cd /var/www/vhosts/helmuga.cloud/tracker.cyclingcloud.com/githubpages/helmugacloud.github.io
-tippecanoe --force --read-parallel --no-feature-limit --no-tile-size-limit --coalesce --simplify-only-low-zooms --minimum-zoom=6 --maximum-zoom=15 --no-tile-compression --include=color --output-to-directory=/var/www/vhosts/helmuga.cloud/tracker.cyclingcloud.com/githubpages/helmugacloud.github.io/2251/tiles /var/www/vhosts/helmuga.cloud/tracker.cyclingcloud.com/replay-map/2251/*.json
-git add --all
-git commit -m "Initial commit"
-git push -u origin main
+/usr/local/bin/tippecanoe --force --read-parallel --no-feature-limit --no-tile-size-limit --coalesce --simplify-only-low-zooms --minimum-zoom=6 --maximum-zoom=15 --no-tile-compression --include=color --output-to-directory=/var/www/vhosts/helmuga.cloud/tracker.cyclingcloud.com/githubpages/helmugacloud.github.io/2251/tiles /var/www/vhosts/helmuga.cloud/tracker.cyclingcloud.com/replay-map/2251/*.json
+/usr/bin/git add --all
+/usr/bin/git commit -m "Initial commit"
+/usr/bin/git push -u origin main
